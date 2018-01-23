52e67e819c0c41441fa22f3cb21afaf9a2fd1810
shucc <cc@cchao.org>
2018-01-23 10:31:29 +0800

    init

---
+++ app/src/main/java/cn/easyar/samples/helloarcloud/MainActivity.java
@@ -0,0 +1,145 @@
+//================================================================================================================================
+//
+//  Copyright (c) 2015-2017 VisionStar Information Technology (Shanghai) Co., Ltd. All Rights Reserved.
+//  EasyAR is the registered trademark or trademark of VisionStar Information Technology (Shanghai) Co., Ltd in China
+//  and other countries for the augmented reality technology developed by VisionStar Information Technology (Shanghai) Co., Ltd.
+//
+//================================================================================================================================
+
+package cn.easyar.samples.helloarcloud;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+
+import java.util.HashMap;
+
+import cn.easyar.Engine;
+
+
+public class MainActivity extends AppCompatActivity {
+    /*
+    * Steps to create the key for this sample:
+    *  1. login www.easyar.com
+    *  2. create app with
+    *      Name: HelloARCloud
+    *      Package Name: cn.easyar.samples.helloarcloud
+    *  3. find the created item in the list and show key
+    *  4. set key string bellow
+    */
+    private static String key = "Etya2911FlNtVwhDnHkgNiKDwjDVrqRIIL6sbw3UqUr8JAPdqzdPiqnIhx9JnaV67hNsxlBevlTITsAo47D8G10ycnfU3MauNqBXyJXJaxlTXhlr4BNsuO3JdTjhhg35AWlPRwxbABPAJXX9A6l7CEnao78I3Xd7CDTDOGX7tV65KH49g0qZdQ1R7TND9n3gQARiU30F";
+    private static String cloud_server_address = "0f14ca21dd9a6f8ff9e150a0a823049b.cn1.crs.easyar.com:8080";
+    private static String cloud_key = "1dd1bef1b0530cce010ab4789b1b41a5";
+    private static String cloud_secret = "34o8luN03SfF3AHJUjWmrUTEJNQEmWB0TKpZ7g7fROUSm0XSpHrrsX6rcyQ09arTBccqnrx5MKmyv1amnCePypUB50FQbHrP7d5kDth49tKfbsQZDuvchNKDl3WNXf0z";
+    private GLView glView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        if (!Engine.initialize(this, key)) {
+            Log.e("HelloAR", "Initialization Failed.");
+        }
+
+        glView = new GLView(this, cloud_server_address, cloud_key, cloud_secret);
+
+        requestCameraPermission(new PermissionCallback() {
+            @Override
+            public void onSuccess() {
+                ((ViewGroup) findViewById(R.id.preview)).addView(glView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            }
+
+            @Override
+            public void onFailure() {
+            }
+        });
+    }
+
+    private interface PermissionCallback {
+        void onSuccess();
+
+        void onFailure();
+    }
+
+    private HashMap<Integer, PermissionCallback> permissionCallbacks = new HashMap<Integer, PermissionCallback>();
+    private int permissionRequestCodeSerial = 0;
+
+    @TargetApi(23)
+    private void requestCameraPermission(PermissionCallback callback) {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                int requestCode = permissionRequestCodeSerial;
+                permissionRequestCodeSerial += 1;
+                permissionCallbacks.put(requestCode, callback);
+                requestPermissions(new String[]{Manifest.permission.CAMERA}, requestCode);
+            } else {
+                callback.onSuccess();
+            }
+        } else {
+            callback.onSuccess();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (permissionCallbacks.containsKey(requestCode)) {
+            PermissionCallback callback = permissionCallbacks.get(requestCode);
+            permissionCallbacks.remove(requestCode);
+            boolean executed = false;
+            for (int result : grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    executed = true;
+                    callback.onFailure();
+                }
+            }
+            if (!executed) {
+                callback.onSuccess();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (glView != null) {
+            glView.onResume();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        if (glView != null) {
+            glView.onPause();
+        }
+        super.onPause();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
