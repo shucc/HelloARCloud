71eb8e352f4640d8c20f834c8be59c3e2dc8d649
shucc <cc@cchao.org>
2018-04-18 11:09:55 +0800

    添加签名文件

--- app/src/main/java/cn/easyar/samples/helloarcloud/App.java
+++ app/src/main/java/cn/easyar/samples/helloarcloud/App.java
@@ -2,8 +2,6 @@ package cn.easyar.samples.helloarcloud;
 
 import android.app.Application;
 
-import com.squareup.leakcanary.LeakCanary;
-
 /**
  * Created by shucc on 18/1/25.
  * cc@cchao.org
@@ -16,10 +14,6 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         instance = this;
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            return;
-        }
-        LeakCanary.install(this);
     }
 
     public static App getInstance() {
